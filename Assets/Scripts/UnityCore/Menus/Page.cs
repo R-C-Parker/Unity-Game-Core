39af8ab596cccf31a39854a30c8fe25ab8be99a1
Reece Parker <R.Parker1471@outlook.com>
2022-03-12 12:48:38 +0000

    Fleshed out the page and pagecontroller scripts

--- Assets/Scripts/UnityCore/Menus/Page.cs
+++ Assets/Scripts/UnityCore/Menus/Page.cs
@@ -12,6 +12,7 @@ namespace UnityCore
             public static readonly string FLAG_NONE = "None";
 
             public PageType type;
+            public bool debug;
             public bool useAnimation;
             public string targetState { get; private set; }
 
@@ -21,6 +22,7 @@ namespace UnityCore
 
             private void OnEnable()
             {
+                CheckAnimatorIntegrity();
             }
 
             #endregion Unity Functions
@@ -29,6 +31,20 @@ namespace UnityCore
 
             public void Animate(bool _on)
             {
+                if (useAnimation)
+                {
+                    m_Animator.SetBool("on", _on);
+
+                    StopCoroutine("AwaitAnimation");
+                    StartCoroutine("AwaitAnimation", _on);
+                }
+                else
+                {
+                    if (!_on)
+                    {
+                        gameObject.SetActive(false);
+                    }
+                }
             }
 
             #endregion Public Functions
@@ -37,19 +53,51 @@ namespace UnityCore
 
             private IEnumerator AwaitAnimation(bool _on)
             {
-                yield return null;
+                targetState = _on ? FLAG_ON : FLAG_OFF;
+
+                /**Wait for the animator to reach the target state */
+                while (!m_Animator.GetCurrentAnimatorStateInfo(0).IsName(targetState))
+                {
+                    yield return null;
+                }
+                /**Wait for the animator to finish animating */
+                while (m_Animator.GetCurrentAnimatorStateInfo(0).normalizedTime < 1)
+                {
+                    yield return null;
+                }
+
+                targetState = FLAG_NONE;
+
+                Log("Page [" + type + "] finished transitioning to " + (_on ? "on" : "off"));
+
+                if (!_on)
+                {
+                    gameObject.SetActive(false);
+                }
             }
 
             private void CheckAnimatorIntegrity()
             {
+                if (useAnimation)
+                {
+                    m_Animator = GetComponent<Animator>();
+                    if (!m_Animator)
+                    {
+                        LogWarning("You chose to animate a page of [" + type + "], but no Animator component exists on the object");
+                    }
+                }
             }
 
             private void Log(string _msg)
             {
+                if (!debug) return;
+                Debug.Log("[Page]: " + _msg);
             }
 
             private void LogWarning(string _msg)
             {
+                if (!debug) return;
+                Debug.Log("[Page]: " + _msg);
             }
 
             #endregion Private Functions
--- Assets/Scripts/UnityCore/Menus/PageController.cs
+++ Assets/Scripts/UnityCore/Menus/PageController.cs
@@ -9,6 +9,7 @@ namespace UnityCore
         public class PageController : MonoBehaviour
         {
             public static PageController instance;
+
             public bool debug;
             public PageType entryPage;
             public Page[] pages;
@@ -19,6 +20,17 @@ namespace UnityCore
 
             private void Awake()
             {
+                /**Check to see if singleton is iniitalised, if not procceeds with initialisation of the instance and other requisite items */
+                if (!instance)
+                {
+                    instance = this;
+                    m_Pages = new Hashtable();
+                    RegisterAllPages();
+                    if (entryPage != PageType.None)
+                    {
+                        TurnPageOn(entryPage);
+                    }
+                }
             }
 
             #endregion Unity Functions
@@ -27,6 +39,18 @@ namespace UnityCore
 
             public void TurnPageOn(PageType _type)
             {
+                if (_type == PageType.None) return;
+                /**If the page the function is attempting to turn on has not been registered, send a warning */
+                if (!PageExists(_type))
+                {
+                    LogWarning("You are attempting to turn a page on [" + _type + "] that has not been registered.");
+                    return;
+                }
+                /**No further _page validation is necessary as it is known from the above that the value is both registered and not null. */
+                Page _page = GetPage(_type);
+                _page.gameObject.SetActive(true);
+                /**If the page has elected to animate, this will use the animator to facilitate that animation, iif the page has no animation there will be no operation */
+                _page.Animate(true);
             }
 
             /**The on parameter allows for an optional second page type to be passed into the function to enable a second parameter.
@@ -34,6 +58,29 @@ namespace UnityCore
 
             public void TurnPageOff(PageType _off, PageType _on = PageType.None, bool _waitForExit = false)
             {
+                if (_off == PageType.None) return;
+                /**If the page the function is attempting to turn off has not been registered, send a warning */
+                if (!PageExists(_off))
+                {
+                    LogWarning("You are attempting to turn a page off [" + _off + "] that has not been registered.");
+                    return;
+                }
+
+                Page _offPage = GetPage(_off);
+                if (_offPage.gameObject.activeSelf)
+                {
+                    _offPage.Animate(false);
+                }
+                Page _onPage = GetPage(_on);
+                if (_waitForExit)
+                {
+                    StopCoroutine("WaitForPageExit");
+                    StartCoroutine(WaitForPageExit(_onPage, _offPage));
+                }
+                else
+                {
+                    TurnPageOn(_on);
+                }
             }
 
             #endregion Public Functions
@@ -42,33 +89,57 @@ namespace UnityCore
 
             private IEnumerator WaitForPageExit(Page _on, Page _off)
             {
-                yield return null;
+                while (_off.targetState != Page.FLAG_NONE)
+                {
+                    yield return null;
+                }
+                TurnPageOn(_on.type);
             }
 
             private void RegisterAllPages()
             {
+                foreach (Page _page in pages)
+                {
+                    RegisterPage(_page);
+                }
             }
 
             private void RegisterPage(Page _page)
             {
+                if (PageExists(_page.type))
+                {
+                    LogWarning("You are attempting to register a page [" + _page.type + "] that has already been registered: " + _page.gameObject.name);
+                    return;
+                }
+                m_Pages.Add(_page.type, _page);
+                Log("Registered new page [" + _page.type + "]");
             }
 
             private Page GetPage(PageType _type)
             {
-                return null;
+                if (!PageExists(_type))
+                {
+                    LogWarning("You are attemptign to get a page [" + _type + "] that has not been registered. ");
+                    return null;
+                }
+                return (Page)m_Pages[_type];
             }
 
             private bool PageExists(PageType _type)
             {
-                return false;
+                return m_Pages.ContainsKey(_type);
             }
 
             private void Log(string _msg)
             {
+                if (!debug) return;
+                Debug.Log("[PageController]: " + _msg);
             }
 
             private void LogWarning(string _msg)
             {
+                if (!debug) return;
+                Debug.Log("[PageController]: " + _msg);
             }
 
             #endregion Private Functions
