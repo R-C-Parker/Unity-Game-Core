ddea5db73280e8b371edd9623c3285467bc5e9ce
Reece Parker <R.Parker1471@outlook.com>
2022-05-27 20:38:34 +0100

    Added partial skeleton for Audio managament functionality

---
+++ Assets/Scripts/UnityCore/Audio/TestAudio.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+namespace UnityCore.Audio
+{
+    public class TestAudio : MonoBehaviour
+    {
+        public AudioController audioController;
+
+        #region Unity Functions
+
+#if UNITY_EDITOR
+
+        private void Update()
+        {
+            if (Input.GetKeyUp(KeyCode.Y))
+            {
+                audioController.PlayAudio(AudioType.ST_01);
+            }
+            if (Input.GetKeyUp(KeyCode.U))
+            {
+                audioController.StopAudio(AudioType.ST_01);
+            }
+            if (Input.GetKeyUp(KeyCode.I))
+            {
+                audioController.RestartAudio(AudioType.ST_01);
+            }
+            if (Input.GetKeyUp(KeyCode.O))
+            {
+                audioController.PlayAudio(AudioType.SFX_01);
+            }
+            if (Input.GetKeyUp(KeyCode.P))
+            {
+                audioController.StopAudio(AudioType.SFX_01);
+            }
+            if (Input.GetKeyUp(KeyCode.L))
+            {
+                audioController.RestartAudio(AudioType.SFX_01);
+            }
+        }
+
+#endif
+
+        #endregion Unity Functions
+    }
+}
